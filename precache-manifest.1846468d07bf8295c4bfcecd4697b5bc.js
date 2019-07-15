75553ce9e5bc24ddde6d92469cf4e39d68b7201c
Sakthivel <sakthivelgovindan@designqubearchitects.com>
2019-07-15 17:45:34 +0530

    Updates

---
+++ precache-manifest.1846468d07bf8295c4bfcecd4697b5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f83bc7a7476e6058a02757a062a399b",
+    "url": "/ssalignment/index.html"
+  },
+  {
+    "revision": "f6ee914ea7ad1a437fbb",
+    "url": "/ssalignment/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ssalignment/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f6ee914ea7ad1a437fbb",
+    "url": "/ssalignment/static/js/main.58959f58.chunk.js"
+  },
+  {
+    "revision": "6b6575c01e7f43d38996",
+    "url": "/ssalignment/static/js/runtime~main.8117bfdb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ssalignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
